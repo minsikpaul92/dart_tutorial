d786f8422b242f2a84e16b8252776d5465d69501
Paul Kim <minsikpaul92@gmail.com>
2026-05-05 15:41:51 -0400

    first commit

---
+++ cli/bin/cli.dart
@@ -0,0 +1,28 @@
+import 'package:cli/cli.dart';
+import 'package:command_runner/command_runner.dart';
+
+void main(List<String> arguments) async {
+  final errorLogger = initFileLogger('errors');
+  final app =
+      CommandRunner(
+          onOutput: (String output) async {
+            await write(output);
+          },
+          onError: (Object error) {
+            if (error is Error) {
+              errorLogger.severe(
+                '[Error] ${error.toString()}\n${error.stackTrace}',
+              );
+              throw error;
+            }
+            if (error is Exception) {
+              errorLogger.warning(error);
+            }
+          },
+        )
+        ..addCommand(HelpCommand())
+        ..addCommand(SearchCommand(logger: errorLogger))
+        ..addCommand(GetArticleCommand(logger: errorLogger));
+
+  await app.run(arguments);
+}
---
+++ command_runner/lib/src/help_command.dart
@@ -0,0 +1,94 @@
+import 'dart:async';
+
+import 'package:command_runner/command_runner.dart';
+
+import 'console.dart';
+import 'exceptions.dart';
+import 'arguments.dart';
+
+// Prints program and argument usage.
+//
+// When given a command as an argument, it prints the usage of
+// that command only, including its options and other details.
+// When the flag 'verbose' is set, it prints options and details for all commands.
+//
+// This command isn't automatically added to CommandRunner instances.
+// Packages users should add it themselves with [CommandRunner.addCommand],
+// or create their own command that prints usage.
+
+class HelpCommand extends Command {
+  HelpCommand() {
+    addFlag(
+      'verbose',
+      abbr: 'v',
+      help: 'When true, this command will print each command and its options.',
+    );
+    addOption(
+      'command',
+      abbr: 'c',
+      help:
+          "When a command is passed as an argument, prints only that command's verbose usage.",
+    );
+  }
+  @override
+  String get name => 'help';
+
+  @override
+  String get description => 'Prints usage information to the command line.';
+
+  @override
+  String? get help => 'Prints this usage information';
+
+  @override
+  FutureOr<Object?> run(ArgResults args) async {
+    final buffer = StringBuffer();
+    buffer.writeln(runner.usage.titleText);
+
+    if (args.flag('verbose')) {
+      for (var cmd in runner.commands){
+        buffer.write(_renderCommandVerbose(cmd));
+      }
+
+      return buffer.toString();
+    }
+
+    if (args.hasOption('command')){
+      var (:option, :input) = args.getOption('command');
+
+      var cmd = runner.commands.firstWhere(
+        (command) => command.name == input,
+        orElse: () {
+          throw ArgumentException(
+            'Input ${args.commandArg} is not a known command.',
+          );
+        },
+      );
+
+      return _renderCommandVerbose(cmd);
+    }
+
+    for (var command in runner.commands) {
+      buffer.writeln(command.usage);
+    }
+
+    return buffer.toString();
+  }
+
+  String _renderCommandVerbose(Command cmd) {
+    final indent = ' ' * 10;
+    final buffer = StringBuffer();
+    buffer.writeln(cmd.usage.instructionText); //abbr, name: description
+    buffer.writeln('$indent ${cmd.help}');
+    if (cmd.valueHelp != null) {
+      buffer.writeln(
+        '$indent [Argument] Required? ${cmd.requiresArgument}, Type: ${cmd.valueHelp}, Default: ${cmd.defaultValue ?? 'none'}',
+      );
+    }
+    buffer.writeln('$indent Options:');
+    for (var option in cmd.options) {
+      buffer.writeln('$indent ${option.usage}');
+    }
+    return buffer.toString();
+  }
+
+}
